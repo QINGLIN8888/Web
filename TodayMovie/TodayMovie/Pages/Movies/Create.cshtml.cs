1cf4065c58e6eb3c578c84e5c6ad360ee10088ec
DESKTOP-EAIJSIR\Qinglin <hwc273228@gmail.com>
2023-12-26 21:44:54 +0800

    使用dotnet aspnet-codegenerator razorpage指令產生建立,讀取,更新,刪除作業頁面

---
+++ TodayMovie/TodayMovie/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPagesMovie.Models;
+using TodayMovie.Data;
+
+namespace TodayMovie.Pages_Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly TodayMovie.Data.TodayMovieContext _context;
+
+        public CreateModel(TodayMovie.Data.TodayMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
